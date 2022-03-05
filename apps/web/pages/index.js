1671505848f22698e35ccf3dd74627dc9df0a98d
Kyle Awayan <kyle@awayan.com>
2022-03-05 15:18:43 -0800

    add map viewer

--- apps/web/pages/index.js
+++ apps/web/pages/index.js
@@ -1,3 +1,32 @@
+import MapViewer from "../components/MapViewer/MapViewer";
+
+const INITIAL_VIEW_STATE = {
+  target: [20000, 20000, 0],
+  zoom: -6,
+};
+
+const ROOT_URL =
+  "https://storage.googleapis.com/ucmerced_bm_maptiles/merced_map";
+const fileName = "merced_map";
+
+function getTooltip({ tile, bitmap }) {
+  if (tile && bitmap) {
+    return `\
+    tile: x: ${tile.x}, y: ${tile.y}, z: ${tile.z}
+    (${bitmap.pixel[0]},${bitmap.pixel[1]}) in ${bitmap.size.width}x${bitmap.size.height}`;
+  }
+  return null;
+}
+
 export default function Home() {
-  return <div>test</div>;
+  return (
+    <div>
+      <MapViewer
+        INITIAL_VIEW_STATE={INITIAL_VIEW_STATE}
+        ROOT_URL={ROOT_URL}
+        fileName={fileName}
+        initZoom={15}
+      />
+    </div>
+  );
 }
