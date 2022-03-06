992209e515e9d6b97901d2a671ddcf19c194309a
Kyle Awayan <kyle@awayan.com>
2022-03-06 01:15:22 -0800

    add api routes

---
+++ apps/web/pages/api/getLocationData.js
@@ -0,0 +1,11 @@
+import db from "./db";
+export default async function handler(req, res) {
+  if (!req.query.location) {
+    req.status(400).json({ error: "no location" });
+  }
+  const response = await db.query("SELECT * FROM data WHERE location_id = $1", [
+    req.query.location,
+  ]);
+  const locations = response.rows;
+  res.status(200).json({ data: locations });
+}
---
+++ apps/web/pages/api/getLocations.js
@@ -0,0 +1,12 @@
+import db from "./db";
+export default async function handler(req, res) {
+  if (!req.query.building) {
+    req.status(400).json({ error: "no building" });
+  }
+  const response = await db.query(
+    "SELECT * FROM locations WHERE building_id = $1",
+    [req.query.building]
+  );
+  const locations = response.rows;
+  res.status(200).json({ locations: locations });
+}
