0e423bc6ee273de5cfb1594b567a9b2ffa0cb346
Kyle Awayan <kyle@awayan.com>
2022-03-06 03:08:47 -0800

    sort by values (free rooms)

--- apps/web/pages/api/getLocationsAndData.js
+++ apps/web/pages/api/getLocationsAndData.js
@@ -6,7 +6,7 @@ export default async function handler(req, res) {
   const response = await db.query(
     `SELECT DISTINCT locations.*, data.* FROM locations, data WHERE building_id = $1 AND
     date = (SELECT MAX(date) FROM data WHERE data.location_id = locations.id)
-    ORDER BY id, date DESC;`,
+    ORDER BY value ASC`,
     [req.query.building]
   );
   const locations = response.rows;
